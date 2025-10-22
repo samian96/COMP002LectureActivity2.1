2d145f6d0e7b7e3a7609888a7a3d040528151771
samian96 <sam.iannazzo@yahoo.com>
2025-10-21 19:18:47 -0700

    console.log results of the operations solved

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log (8 * null); // 0
+console.log ("5" - 1) // 4
+console.log ("5" + 1) // 51
+console.log ("five" * 2) // NaN
+console.log (false == 0 =) // True
+console.log (5 > 2 && 6 > 3 =) // True
+console.log (false == false = true . "equal" : "not equal" =) // equal
+console.log (true || false =) // true
