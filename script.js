329795d36925cdf7b53b8537c435ee2152c142b2
samuel Iannazzo <sam.iannazzo@yahoo.com>
2025-10-22 18:55:38 -0700

    updated during lecture wednesday /

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-console.log (8 * null); // 0
-console.log ("5" - 1) // 4
-console.log ("5" + 1) // 51
-console.log ("five" * 2) // NaN
-console.log (false == 0 =) // True
-console.log (5 > 2 && 6 > 3 =) // True
-console.log (false == false = true . "equal" : "not equal" =) // equal
-console.log (true || false =) // true
+// A single line comment
+/*
+* This
+* is
+* multi-line
+* comment*/
+
+// Value, Types, and Operators
+console.log("Value, Types, and Operators")
+// boolean
+console.log("****************************")
+console.log("Boolean")
+console.log(true) // example of boolean with true value
+console.log(false) // example of a boolean with a false value
+// null
+console.log("****************************")
+console.log("Nulls")
+console.log(null) // example of a null
+// undefined
+console.log("****************************")
+console.log("undefined")
+let myNumber;
+console.log(myNumber); // example of an undefined number
+// number
+console.log("****************************")
+console.log("Numbers")
+console.log(15) // example of a whole number
+console.log(2.22) // example of a floating point number
+console.log(5e5) // example of an exponent
+// string
+console.log("****************************")
+console.log("String")
+console.log("Hello, World1!") // example of a string inside double quotes
+console.log('Hello, World2!') // example of a string inside single quotes
+console.log(`Hello, World3!`) // example of a string inside backkicks quotes
