bbf3e6e413229b63b8d2bc5df07fdbae0be15aee
samuel Iannazzo <sam.iannazzo@yahoo.com>
2025-10-22 19:00:34 -0700

    added special characters \n \t

--- script.js
+++ script.js
@@ -6,30 +6,35 @@
 * comment*/
 
 // Value, Types, and Operators
-console.log("Value, Types, and Operators")
+console.log("Value, Types, and Operators");
 // boolean
-console.log("****************************")
-console.log("Boolean")
-console.log(true) // example of boolean with true value
-console.log(false) // example of a boolean with a false value
+console.log("****************************");
+console.log("Boolean");
+console.log(true) ;// example of boolean with true value
+console.log(false); // example of a boolean with a false value
 // null
-console.log("****************************")
-console.log("Nulls")
-console.log(null) // example of a null
+console.log("****************************");
+console.log("Nulls");
+console.log(null); // example of a null
 // undefined
-console.log("****************************")
-console.log("undefined")
+console.log("****************************");
+console.log("undefined");
 let myNumber;
 console.log(myNumber); // example of an undefined number
 // number
-console.log("****************************")
-console.log("Numbers")
-console.log(15) // example of a whole number
-console.log(2.22) // example of a floating point number
-console.log(5e5) // example of an exponent
+console.log("****************************");
+console.log("Numbers");
+console.log(15); // example of a whole number
+console.log(2.22); // example of a floating point number
+console.log(5e5); // example of an exponent
 // string
-console.log("****************************")
-console.log("String")
-console.log("Hello, World1!") // example of a string inside double quotes
-console.log('Hello, World2!') // example of a string inside single quotes
-console.log(`Hello, World3!`) // example of a string inside backkicks quotes
+console.log("****************************");
+console.log("String");
+console.log("Hello, World1!"); // example of a string inside double quotes
+console.log('Hello, World2!'); // example of a string inside single quotes
+console.log(`Hello, World3!`) ;// example of a string inside backkicks quotes
+// Strings special characters
+console.log("****************************");
+console.log("Special characters when manipulating strings");
+console.log("\nHello"); // \n creates a new line
+console.log("\tHello"); // \t creates a space infront of the line
